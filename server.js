1ad341945c4fdac2f285e64b7cde2d9baa7b7b95
Mickael <monemail@gmail.com>
2025-09-10 09:52:58 +0200

    second commit

--- server.js
+++ server.js
@@ -3,50 +3,53 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-let tasks = [];
+let tasks = [
+    { id: 1, title: 'Tâche 1', completed: false },
+    { id: 2, title: 'Tâche 2', completed: true },
+    { id: 3, title: 'Tâche 3', completed: false },
+];
 
 // read tasks
 app.get('/tasks', (req, res) => {
     if (tasks.length === 0) {
         return res.send("Aucune tâche trouvée");
     }
-    res.send(tasks);
-
+    res.json(tasks);
 });
 
 // create a task
-app.post('/tasks/add', (req, res) => {
+app.post('/tasks', (req, res) => {
     const task = {
-        id: req.params.id,
-        title: req.params.title,
-        completed: req.params.completed || false,
+        id: tasks.length + 1,
+        title: req.body.title,
+        completed:  false,
     };
     tasks.push(task);
-    res.send( `Tâche ${title} créée avec succès`)
+    return res.status(201).json(task);
 });
 // update a task
-app.put('/tasks/update/:id', (req, res) => {
+app.put('/tasks/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const task = tasks.find(t => t.id === id);
     if (!task) {
         return res.status(404).send("Tâche non trouvée");
     }
-    task.title = req.params.title || task.title;
-    task.completed = req.params.completed || task.completed;
-    res.send(task);
+    task.title = req.body.title || task.title;
+    task.completed = req.body.completed || task.completed;
+    return res.json(task);
 });
-// delete a task
-app.delete('/tasks/delete/:id', (req, res) => {
+    // delete a task
+    app.delete('/tasks/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const index = tasks.find(t => t.id === id);
     if (index === -1) {
-        return res.status(404).send("Tâche non trouvée");
+        return res.status(404).json("Tâche non trouvée");
     }
     tasks.splice(index, 1);
-    res.status(204).send();
+    return res.status(200).json();
 });
 
 // run server on port 3000
